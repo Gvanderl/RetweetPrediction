f5483f1ecd46e4d126f60907cca332e9457a9a9b
Juliete <juliette.rossie@gmail.com>
2020-12-03 14:00:57 +0100

    first commit

---
+++ process_with_all_cols.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import logging
+from dataprocessor import DataProcessor
+from config import *
+
+data_processor = DataProcessor()
+data_processor.apply_glove(False, 100)
+
+print("shape = ", data_processor.df.shape)
+
+X_train, X_test, y_train, y_test = data_processor.get_split_df_with_all_cols()
+
+train_df = pd.DataFrame({"timestamp": X_train["timestamp"], "user_verified": X_train["user_verified"], "user_statuses_count": X_train["user_statuses_count"], "user_followers_count": X_train["user_followers_count"], "user_friends_count": X_train["user_friends_count"],  "text": X_train["text"], "labels": y_train})
+eval_df = pd.DataFrame({"timestamp": X_test["timestamp"], "user_verified": X_test["user_verified"], "user_statuses_count": X_test["user_statuses_count"], "user_followers_count": X_test["user_followers_count"], "user_friends_count": X_test["user_friends_count"],  "text": X_test["text"], "labels": y_test})
+
+print(train_df.shape)
+print(train_df["text"][0])
+
+train_df.to_hdf(data_folder / "train.h5", "train")
+eval_df.to_hdf(data_folder / "eval.h5", "eval")
+
+print("save done")
